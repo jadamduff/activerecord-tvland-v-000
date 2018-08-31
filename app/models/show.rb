67dbe4cc01e5f7d348fa6553edf96a5c6144f9ac
Adam Duff <j.adam.duff@gmail.com>
2018-08-31 09:43:29 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+
+  def build_network(attr = {})
+    Network.create do |x|
+      x.call_letters = "Fox"
+    end
+  end
 end
